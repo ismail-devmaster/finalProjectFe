0fdb1ef98bc4f1568a4e0edef0cc857f5830b6e9
xCyberpunk <peeplil6666@gmail.com>
2025-03-11 00:32:22 +0100

    admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,5 @@
 "use client";
-
 import type React from "react";
-
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { Loader2, Trash2, Search } from "lucide-react";
