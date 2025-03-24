c300eb7ee565acee845d4ba3a395d340c314ac29
ismail-devmaster <ismail.devwebmobile@gmail.com>
2025-03-24 03:18:55 +0100

    eslint ignoreDuringBuilds true

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,22 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable warnings about the explicit use of 'any'
+      "@typescript-eslint/no-explicit-any": "off",
+      // Disable warnings for unused variables
+      "@typescript-eslint/no-unused-vars": "off",
+      // Disable warnings regarding missing or complex dependencies in useEffect hooks
+      "react-hooks/exhaustive-deps": "off",
+      // Other previously disabled rules
+      "react/no-unescaped-entities": "off",
+      "react-hooks/rules-of-hooks": "off",
+      "@next/next/no-html-link-for-pages": "off",
+      "@next/next/no-img-element": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
